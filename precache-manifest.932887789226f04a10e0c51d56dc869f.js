0495770daeb370f93117f729e43ef2b79801f755
Code Sandbox <hello@codesandbox.io>
2021-09-13 23:14:22 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.932887789226f04a10e0c51d56dc869f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfde263f4c7058f8f4db",
+    "url": "https://.github.io/csb-rf865/static/css/main.190fc26e.chunk.css"
+  },
+  {
+    "revision": "dfde263f4c7058f8f4db",
+    "url": "https://.github.io/csb-rf865/static/js/main.5678ef20.chunk.js"
+  },
+  {
+    "revision": "092a92412b939fb666c6",
+    "url": "https://.github.io/csb-rf865/static/js/runtime~main.922fd271.js"
+  },
+  {
+    "revision": "fc80dff9ab57d946ad04",
+    "url": "https://.github.io/csb-rf865/static/js/2.9928d165.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https://.github.io/csb-rf865/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "11832535819f208f8b6b155e7a905ed9",
+    "url": "https://.github.io/csb-rf865/index.html"
+  }
+];
